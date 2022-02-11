23e598e3539cc8d72a6a119e29ad68e8fab3d25e
vikcy10 <99043048+vikcy10@users.noreply.github.com>
2022-02-11 11:29:40 -0800

    Create blocks.js

---
+++ blocks.js
@@ -0,0 +1,60 @@
+//What  Action Type  Left/R Up/D Front/B
+blocks.place(BEDROCK, pos(0, 0, 1))
+blocks.place(BEDROCK, pos(0, 0, 2))
+blocks.place(BEDROCK, pos(0, 0, 3))
+blocks.place(BEDROCK, pos(0, 0, 4))
+blocks.place(BEDROCK, pos(0, 0, 5))
+
+blocks.place(BEDROCK, pos(-1, 0, 1))
+blocks.place(BEDROCK, pos(-1, 0, 2))
+blocks.place(BEDROCK, pos(-1, 0, 3))
+blocks.place(BEDROCK, pos(-1, 0, 4))
+blocks.place(BEDROCK, pos(-1, 0, 5))
+
+blocks.place(BEDROCK, pos(-2, 0, 1))
+blocks.place(BEDROCK, pos(-2, 0, 2))
+blocks.place(BEDROCK, pos(-2, 0, 3))
+blocks.place(BEDROCK, pos(-2, 0, 4))
+blocks.place(BEDROCK, pos(-2, 0, 5))
+
+blocks.place(BEDROCK, pos(2, 0, 1))
+blocks.place(BEDROCK, pos(2, 0, 2))
+blocks.place(BEDROCK, pos(2, 0, 3))
+blocks.place(BEDROCK, pos(2, 0, 4))
+blocks.place(BEDROCK, pos(2, 0, 5))
+
+blocks.place(BEDROCK, pos(-2, 1, 1))
+blocks.place(BEDROCK, pos(-2, 1, 2))
+blocks.place(BEDROCK, pos(-2, 1, 3))
+blocks.place(BEDROCK, pos(-2, 1, 4))
+blocks.place(BEDROCK, pos(-2, 1, 5))
+
+blocks.place(BEDROCK, pos(-2, 2, 1))
+blocks.place(BEDROCK, pos(-2, 2, 2))
+blocks.place(BEDROCK, pos(-2, 2, 3))
+blocks.place(BEDROCK, pos(-2, 2, 4))
+blocks.place(BEDROCK, pos(-2, 2, 5))
+
+blocks.place(BEDROCK, pos( -2, 3, 1))
+blocks.place(BEDROCK, pos( -2, 3, 2))
+blocks.place(BEDROCK, pos( -2, 3, 3))
+blocks.place(BEDROCK, pos( -2, 3, 4))
+blocks.place(BEDROCK, pos( -2, 3, 5))
+
+blocks.place(BEDROCK, pos(1, 0, 1))
+blocks.place(BEDROCK, pos(1, 0, 2))
+blocks.place(BEDROCK, pos(1, 0, 3))
+blocks.place(BEDROCK, pos(1, 0, 4))
+blocks.place(BEDROCK, pos(1, 0, 5))
+
+
+
+
+
+
+
+
+
+
+
+
